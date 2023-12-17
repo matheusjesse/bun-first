936ceb7e7e3cd24bd7e7839464b9950fd27c49fa
matheus jesse <matheus_jesse@hotmail.com>
2023-12-16 21:37:07 -0300

    feat: criando a primeira conexao

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
-console.log("Hello via Bun!");
+const server = Bun.serve({
+    port: 3000,
+    fetch(req){
+        return new Response("Hello world!");
+    }
+});
+
+console.log(`Listening on PORT http://localhost:${server.port}`)
