b282817e64626e82a894230a7e92e296a86365c7
matheus jesse <matheus_jesse@hotmail.com>
2023-12-16 21:53:33 -0300

    feat: criando rotas

--- index.ts
+++ index.ts
@@ -3,8 +3,22 @@ import figlet from "figlet";
 const server = Bun.serve({
     port: 3000,
     fetch(req){
-        const body = figlet.textSync("Hola!")
-        return new Response(body);
+
+        const url = new URL(req.url)
+        
+        if(url.pathname === '/'){
+            const body = figlet.textSync("Hola!")
+            return new Response(body);
+        }
+        if(url.pathname === '/about'){
+            return new Response("about me!")
+        }
+
+        if(url.pathname === '/contact'){
+            return new Response("contac us!")
+        }
+
+        return new Response('404');
     }
 });
 
